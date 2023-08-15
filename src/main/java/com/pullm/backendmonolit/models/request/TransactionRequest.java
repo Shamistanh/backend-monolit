e25b423665c496f1c2077180a5f7ae47f4639238
Agil Aghamirzayev <agila@goldenpay.az>
2023-08-15 16:12:38 +0400

    Optimize imports

--- src/main/java/com/pullm/backendmonolit/models/request/TransactionRequest.java
+++ src/main/java/com/pullm/backendmonolit/models/request/TransactionRequest.java
@@ -1,12 +1,12 @@
 package com.pullm.backendmonolit.models.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 @Builder
@@ -14,7 +14,8 @@ import org.springframework.format.annotation.DateTimeFormat;
 @AllArgsConstructor
 public class TransactionRequest {
     private String storeName;
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", shape = JsonFormat.Shape.STRING)
     private LocalDateTime date;
     private List<ProductRequest> products;
+
 }
